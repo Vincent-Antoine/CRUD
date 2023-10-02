bfa2664671e804dc4f98f504b1b28e77981e9ab5
Vincent-Antoine <vincentcomparato69@gmail.com>
2023-10-02 18:33:01 +0200

    connexion a la bdd et création de table personalité

---
+++ src/Repository/PersonnaliteRepository.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Personnalite;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Personnalite>
+ *
+ * @method Personnalite|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Personnalite|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Personnalite[]    findAll()
+ * @method Personnalite[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class PersonnaliteRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Personnalite::class);
+    }
+
+//    /**
+//     * @return Personnalite[] Returns an array of Personnalite objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('p')
+//            ->andWhere('p.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('p.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Personnalite
+//    {
+//        return $this->createQueryBuilder('p')
+//            ->andWhere('p.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
